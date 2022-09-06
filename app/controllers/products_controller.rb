a17ca27d285febebbe2a363a367231967cea8fd7
CristianRojasBetancur <cristianrojasbetancur@gmail.com>
2022-09-06 15:21:00 -0500

    Add the post new product function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,25 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
+  end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "Producto creado con éxito."
+    else
+      @message = "Error al crear el producto"
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :url, :description)
   end
 end
